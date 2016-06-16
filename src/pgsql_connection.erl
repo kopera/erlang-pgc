c8f3d42426d1c0d6d7abc6b815124d8b07136c36
Ali Sabil <ali.sabil@gmail.com>
2016-06-16 19:00:46 +0200

    Implement query execution

--- src/pgsql_connection.erl
+++ src/pgsql_connection.erl
@@ -1,6 +1,14 @@
 -module(pgsql_connection).
 -export([
-    start_link/2
+    start_link/2,
+    stop/1
+]).
+
+-export([
+    prepare/3,
+    unprepare/3,
+    execute/4,
+    transaction/3
 ]).
 
 -behaviour(gen_db_client_connection).
@@ -14,30 +22,46 @@
     handle_rollback/2,
     handle_prepare/3,
     handle_execute/4,
-    handle_query/4,
     handle_unprepare/3,
     handle_info/2,
     disconnect/2
 ]).
 
 -include("./pgsql_protocol_messages.hrl").
--define(recv_timeout, 5000).
+-define(recv_timeout, infinity).
 -record(state, {
     transport :: pgsql_transport:transport(),
     backend_key :: {pos_integer(), pos_integer()},
     parameters :: #{binary() => binary()},
+    codec :: pgsql_codec:codec(),
     buffer = <<>> :: binary()
 }).
 
--record(query, {
+-record(statement, {
     name = <<>> :: binary(),
-    parameters = [] :: [pgsql_protocol:oid()],
+    parameters = [] :: [pgsql_types:oid()],
     fields = [] :: [#msg_row_description_field{}]
 }).
 
 
 start_link(TransportOpts, DatabaseOpts) ->
     gen_db_client_connection:start_link(?MODULE, {TransportOpts, DatabaseOpts}).
+
+stop(Conn) ->
+    gen_db_client_connection:stop(Conn).
+
+prepare(Conn, Query, Opts) ->
+    gen_db_client_connection:prepare(Conn, Query, Opts).
+
+unprepare(Conn, PreparedQuery, Opts) ->
+    gen_db_client_connection:unprepare(Conn, PreparedQuery, Opts).
+
+execute(Conn, Query, Params, Opts) ->
+    gen_db_client_connection:execute(Conn, Query, Params, Opts).
+
+transaction(Conn, Fun, Opts) ->
+    gen_db_client_connection:transaction(Conn, Fun, Opts).
+
 
 %% @hidden
 connect({TransportOpts, DatabaseOpts}) ->
@@ -87,7 +111,8 @@ configure(Transport, BackendKey, Parameters) ->
             {ok, #state{
                 transport = Transport1,
                 backend_key = BackendKey,
-                parameters = Parameters
+                parameters = Parameters,
+                codec = pgsql_codec:new(Parameters, #{})
             }};
         {ok, #msg_backend_key_data{id = Id, secret = Secret}, Transport1} ->
             configure(Transport1, {Id, Secret}, Parameters);
@@ -123,7 +148,7 @@ handle_rollback(_Opts, State) ->
     {error, not_implemented, State}.
 
 handle_prepare({Name, Statement}, _Opts, #state{transport = Transport} = State) ->
-    case prepare(Transport, Name, Statement) of
+    case do_prepare(Transport, Name, Statement) of
         {done, {ok, Reply}, Transport1} ->
             {ok, Reply, State#state{transport = Transport1}};
         {done, {error, Reason}, Transport1} ->
@@ -132,93 +157,192 @@ handle_prepare({Name, Statement}, _Opts, #state{transport = Transport} = State) 
             {disconnect, Reason}
     end.
 
-handle_execute(_PreparedQuery, _Params, _Opts, State) ->
-    {error, not_implemented, State}.
+handle_execute(#statement{name = Name, parameters = ParamTypes, fields = FieldsDesc}, Params, _Opts, State) ->
+    case length(ParamTypes) =:= length(Params) of
+        true ->
+            FieldsNames = [FieldName || #msg_row_description_field{name = FieldName} <- FieldsDesc],
+            FieldsTypes = [FieldType || #msg_row_description_field{type_oid = FieldType} <- FieldsDesc],
+            {Codec, Transport} = codec_sync(State#state.transport, State#state.codec, ParamTypes ++ FieldsTypes),
+            case do_execute(Transport, Name, codec_encode(ParamTypes, Params, Codec)) of
+                {done, {ok, Tag, Rows}, Transport1} ->
+                    DecodedRows = [codec_decode(FieldsTypes, FieldsValues, Codec) || FieldsValues <- Rows],
+                    {ok, {ok, Tag, FieldsNames, DecodedRows}, State#state{transport = Transport1, codec = Codec}};
+                {done, {error, Reason}, Transport1} ->
+                    {error, Reason, State#state{transport = Transport1, codec = Codec}};
+                {disconnected, Reason} ->
+                    {disconnect, Reason}
+            end;
+        false ->
+            {error, invalid_params_length, State}
+    end;
+handle_execute(Statement, Params, Opts, #state{transport = Transport} = State) ->
+    case do_prepare(Transport, "", Statement) of
+        {done, {ok, PreparedStatement}, Transport1} ->
+            handle_execute(PreparedStatement, Params, Opts, State#state{transport = Transport1});
+        {done, {error, Reason}, Transport1} ->
+            {error, Reason, State#state{transport = Transport1}};
+        {disconnected, Reason} ->
+            {disconnect, Reason}
+    end.
 
-handle_unprepare(_PreparedQuery, _Opts, State) ->
-    {error, not_implemented, State}.
-
-handle_query(Query, Params, Opts, State) ->
-    {error, not_implemented, State}.
+handle_unprepare(#statement{name = Name}, _Opts, #state{transport = Transport} = State) ->
+    case do_unprepare(Transport, Name) of
+        {done, ok, Transport1} ->
+            {ok, State#state{transport = Transport1}};
+        {done, {error, Reason}, Transport1} ->
+            {error, Reason, State#state{transport = Transport1}};
+        {disconnected, Reason} ->
+            {disconnect, Reason}
+    end.
 
 handle_info(_Msg, State) ->
     {ok, State}.
 
-disconnect(_Info, #state{transport = Transport} = State) ->
+disconnect(_Info, #state{transport = Transport}) ->
+    _ = pgsql_transport:send(Transport, [#msg_terminate{}]),
     _ = pgsql_transport:close(Transport),
     ok.
 
 
+%% Codec
+
+codec_sync(Transport, Codec, Types) ->
+    case pgsql_codec:has_types(Types, Codec) of
+        true ->
+            {Transport, Codec};
+        false ->
+            {TypeInfos, Transport1} = pgsql_types:load(Transport),
+            {pgsql_codec:update_types(TypeInfos, Codec), Transport1}
+    end.
+
+codec_encode(Types, Values, Codec) ->
+    lists:zipwith(fun
+        (_Type, null) -> {binary, null};
+        (Type, Value) -> {binary, pgsql_codec:encode(Type, Value, Codec)}
+    end, Types, Values).
+
+codec_decode(Types, Values, Codec) ->
+    lists:zipwith(fun
+        (_Type, null) -> null;
+        (Type, Value) -> pgsql_codec:decode(Type, Value, Codec)
+    end, Types, Values).
+
+
 %% Prepare protocol
 
-prepare(Transport, Name, Statement) ->
+do_prepare(Transport, Name, Statement) ->
     Messages = [
         #msg_parse{name = Name, statement = Statement},
         #msg_describe{type = statement, name = Name},
         #msg_sync{}
     ],
     case pgsql_transport:send(Transport, Messages) of
-        ok -> prepare_parsing(Transport, Name);
+        ok -> do_prepare_parsing(Transport, Name);
         {error, _} = Error -> {disconnect, Error}
     end.
 
-prepare_parsing(Transport, Name) ->
+do_prepare_parsing(Transport, Name) ->
     case pgsql_transport:recv(Transport, ?recv_timeout) of
         {ok, #msg_parse_complete{}, Transport1} ->
-            prepare_describing_parameters(Transport1, Name);
-        {ok, #msg_error_response{fields = Details}} ->
-            sync(Transport, {error, Details});
+            do_prepare_describing_parameters(Transport1, Name);
+        {ok, #msg_error_response{fields = Details}, Transport1} ->
+            do_sync(Transport1, {error, Details});
         {error, _} = Error ->
             {disconnected, Error}
     end.
 
-prepare_describing_parameters(Transport, Name) ->
+do_prepare_describing_parameters(Transport, Name) ->
     case pgsql_transport:recv(Transport, ?recv_timeout) of
         {ok, #msg_parameter_description{types = ParametersTypes}, Transport1} ->
-            prepare_describing_row(Transport1, Name, ParametersTypes);
-        {ok, #msg_error_response{fields = Details}} ->
-            sync(Transport, {error, Details});
+            do_prepare_describing_row(Transport1, Name, ParametersTypes);
+        {ok, #msg_error_response{fields = Details}, Transport1} ->
+            do_sync(Transport1, {error, Details});
         {error, _} = Error ->
             {disconnected, Error}
     end.
 
-prepare_describing_row(Transport, Name, ParametersTypes) ->
+do_prepare_describing_row(Transport, Name, ParametersTypes) ->
     case pgsql_transport:recv(Transport, ?recv_timeout) of
-        {ok, #msg_row_description{fields = RowDescription}, Transport1} ->
-            sync(Transport1, {ok, #query{name = Name, parameters = ParametersTypes, fields = RowDescription}});
-        {ok, #msg_no_data{}} ->
-            sync(Transport, {ok, #query{name = Name, parameters = ParametersTypes}});
+        {ok, #msg_row_description{fields = Fields}, Transport1} ->
+            do_sync(Transport1, {ok, #statement{name = Name, parameters = ParametersTypes, fields = Fields}});
+        {ok, #msg_no_data{}, Transport1} ->
+            do_sync(Transport1, {ok, #statement{name = Name, parameters = ParametersTypes}});
+        {error, _} = Error ->
+            {disconnected, Error}
+    end.
+
+%% Unprepare protocol
+
+do_unprepare(Transport, Name) ->
+    Messages = [
+        #msg_close{type = statement, name = Name},
+        #msg_sync{}
+    ],
+    case pgsql_transport:send(Transport, Messages) of
+        ok -> do_unprepare_closing(Transport);
+        {error, _} = Error -> {disconnect, Error}
+    end.
+
+do_unprepare_closing(Transport) ->
+    case pgsql_transport:recv(Transport, ?recv_timeout) of
+        {ok, #msg_close_complete{}, Transport1} ->
+            do_sync(Transport1, ok);
+        {ok, #msg_error_response{fields = Details}, Transport1} ->
+            do_sync(Transport1, {error, Details});
         {error, _} = Error ->
             {disconnected, Error}
     end.
 
 %% Execute protocol
-execute(Transport, #query{name = Name, parameters = ParametersTypes, fields = FieldsTypes}, Parameters, Types) ->
-    ok = pgsql_transport:send(Transport, [
+
+do_execute(Transport, Name, Params) ->
+    Messages = [
         #msg_bind{
             statement = Name,
             portal = "",
-            parameters = lists:zipwith(fun
-                (null, _) ->
-                    {binary, null};
-                (Parameter, ParameterType) ->
-                    {binary, pgsql_query:encode({Parameter, ParameterType}, Types, [])}
-            end, Parameters, ParametersTypes),
+            parameters = Params,
             results = [binary]
         },
         #msg_execute{portal = "", limit = 0},
         #msg_sync{}
-    ]),
-    execute_binding(Connection, RowDescription, Types).
+    ],
+    case pgsql_transport:send(Transport, Messages) of
+        ok -> do_execute_binding(Transport);
+        {error, _} = Error -> {disconnect, Error}
+    end.
+
+do_execute_binding(Transport) ->
+    case pgsql_transport:recv(Transport, ?recv_timeout) of
+        {ok, #msg_bind_complete{}, Transport1} ->
+            do_execute_receiving_rows(Transport1, []);
+        {ok, #msg_error_response{fields = Details}, Transport1} ->
+            do_sync(Transport1, {error, Details});
+        {error, _} = Error ->
+            {disconnected, Error}
+    end.
+
+do_execute_receiving_rows(Transport, Acc) ->
+    case pgsql_transport:recv(Transport, ?recv_timeout) of
+        {ok, #msg_no_data{}, Transport1} ->
+            do_execute_receiving_rows(Transport1, []);
+        {ok, #msg_data_row{values = Values}, Transport1} ->
+            do_execute_receiving_rows(Transport1, [Values | Acc]);
+        {ok, #msg_command_complete{tag = Tag}, Transport1} ->
+            do_sync(Transport1, {ok, Tag, lists:reverse(Acc)});
+        {ok, #msg_error_response{fields = Details}, Transport1} ->
+            do_sync(Transport1, {error, Details});
+        {error, _} = Error ->
+            {disconnected, Error}
+    end.
 
 %% Sync protocol
 
-sync(Transport, Result) ->
+do_sync(Transport, Result) ->
     case pgsql_transport:recv(Transport, ?recv_timeout) of
         {ok, #msg_ready_for_query{}, Transport1} ->
-            {done, Result, Transport};
+            {done, Result, Transport1};
         {ok, _, Transport1} ->
-            sync(Transport1, Result);
+            do_sync(Transport1, Result);
         {error, _} = Error ->
             {disconnected, Error}
     end.
