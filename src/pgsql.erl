654f2baec6d11beb7bd1ec5de95bb72f6db80593
Ali Sabil <ali.sabil@gmail.com>
2016-06-23 16:23:35 +0200

    Add the connection options to the pool API

--- src/pgsql.erl
+++ src/pgsql.erl
@@ -4,8 +4,8 @@
     activity/3
 ]).
 -export([
-    start_link/4,
-    child_spec/5
+    start_link/5,
+    child_spec/6
 ]).
 
 -behaviour(supervisor).
@@ -29,35 +29,37 @@ activity(Name, Fun, Timeout) ->
             exit(Error)
     end.
 
--spec start_link(Name, PoolOpts, TransportOpts, DatabaseOpts) -> {ok, pid()} | {error, any()} when
+-spec start_link(Name, PoolOpts, TransportOpts, DatabaseOpts, ConnectionOpts) -> {ok, pid()} | {error, any()} when
     Name :: pgsql_connections:name(),
     PoolOpts :: pgsql_connections:start_options(),
     TransportOpts :: pgsql_connection:transport_options(),
-    DatabaseOpts :: pgsql_connection:database_options().
-start_link(Name, PoolOpts, TransportOpts, DatabaseOpts) ->
-    supervisor:start_link(?MODULE, {pgsql_sup, [Name, PoolOpts, TransportOpts, DatabaseOpts]}).
+    DatabaseOpts :: pgsql_connection:database_options(),
+    ConnectionOpts :: pgsql_connection:connection_options().
+start_link(Name, PoolOpts, TransportOpts, DatabaseOpts, ConnectionOpts) ->
+    supervisor:start_link(?MODULE, {pgsql_sup, [Name, PoolOpts, TransportOpts, DatabaseOpts, ConnectionOpts]}).
 
--spec child_spec(Id, Name, PoolOpts, TransportOpts, DatabaseOpts) -> supervisor:child_spec() when
+-spec child_spec(Id, Name, PoolOpts, TransportOpts, DatabaseOpts, ConnectionOpts) -> supervisor:child_spec() when
     Id :: any(),
     Name :: pgsql_connections:name(),
     PoolOpts :: pgsql_connections:start_options(),
     TransportOpts :: pgsql_connection:transport_options(),
-    DatabaseOpts :: pgsql_connection:database_options().
-child_spec(Id, Name, PoolOpts, TransportOpts, DatabaseOpts) ->
+    DatabaseOpts :: pgsql_connection:database_options(),
+    ConnectionOpts :: pgsql_connection:connection_options().
+child_spec(Id, Name, PoolOpts, TransportOpts, DatabaseOpts, ConnectionOpts) ->
     #{
         id => Id,
         type => supervisor,
-        start => {supervisor, start_link, [?MODULE, {pgsql_sup, [Name, PoolOpts, TransportOpts, DatabaseOpts]}]}
+        start => {supervisor, start_link, [?MODULE, {pgsql_sup, [Name, PoolOpts, TransportOpts, DatabaseOpts, ConnectionOpts]}]}
     }.
 
 %% @hidden
-init({pgsql_sup, [Name, PoolOpts, TransportOpts, DatabaseOpts]}) ->
+init({pgsql_sup, [Name, PoolOpts, TransportOpts, DatabaseOpts, ConnectionOpts]}) ->
     Flags = #{strategy => one_for_all},
     Children = [
         #{
             id => connections_sup,
             type => supervisor,
-            start => {supervisor, start_link, [?MODULE, {connections_sup, [TransportOpts, DatabaseOpts]}]}
+            start => {supervisor, start_link, [?MODULE, {connections_sup, [TransportOpts, DatabaseOpts, ConnectionOpts]}]}
         },
         #{
             id => connections,
@@ -65,12 +67,12 @@ init({pgsql_sup, [Name, PoolOpts, TransportOpts, DatabaseOpts]}) ->
         }
     ],
     {ok, {Flags, Children}};
-init({connections_sup, [TransportOpts, DatabaseOpts]}) ->
+init({connections_sup, [TransportOpts, DatabaseOpts, ConnectionOpts]}) ->
     Flags = #{strategy => simple_one_for_one},
     Children = [
         #{
             id => connection,
-            start => {pgsql_connection, start_link, [TransportOpts, DatabaseOpts]},
+            start => {pgsql_connection, start_link, [TransportOpts, DatabaseOpts, ConnectionOpts]},
             restart => temporary
         }
     ],
