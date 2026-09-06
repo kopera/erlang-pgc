fa1e1f8dcafd21857e93bcf4e3901bdfac86e8ea
Ali Sabil <ali.sabil@kopera.com>
2026-09-06 14:11:57 +0200

    client: types: make type opaque

--- src/pgc_client_types.erl
+++ src/pgc_client_types.erl
@@ -11,7 +11,7 @@
     t/0
 ]).
 
--type t() :: ets:table().
+-opaque t() :: ets:table().
 
 -type id() :: pgc_protocol:oid().
 -type kind() :: base | composite | domain | enum | pseudo | range | multirange | other.
